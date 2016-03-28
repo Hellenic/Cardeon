411c77677f950cfc84d4e1c515f6933622656bf7
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2016-03-28 11:14:51 +0300

    Moved hexagon grid to it's own library

--- game/modules/components/HexagonGrid/index.js
+++ game/modules/components/HexagonGrid/index.js
@@ -1,43 +1,21 @@
 import React from 'react';
-import { Grid, Hex, Layout } from './Hexlib/';
+import { HexGrid } from 'react-hexgrid';
 
 class HexagonGrid extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      slots: []
-    };
-  }
-
-  componentWillMount() {
-    let hexagons = [];
-    let layout = new Layout(10, 10, false);
-
-    this.props.hexas.forEach(hex => {
-      hexagons.push(hex);
-    });
-
-    this.setState({
-      hexagons: hexagons,
-      layout: layout
-    });
-  }
 
   render() {
-    return (
-      <div>
-        <Grid width={800} height={600} hexagons={this.state.hexagons} layout={this.state.layout} />
-      </div>
-    );
-  }
-}
 
-HexagonGrid.defaultProps = {
-  hexas: [
-    new Hex(0, -1, 0), new Hex(1, -1, -1),
-    new Hex(-1, 0, 1), new Hex(0, 0, 0), new Hex(1, 0, -1),
-    new Hex(-1, 1, 1), new Hex(0, 1, 0)
-  ]
+    let config = {
+      width: 1400,
+      height: 800,
+      layoutSize: {width: 10, height: 10},
+      flat: true,
+      origin: {x: 0, y: 0}
+    };
+
+    return (<HexGrid {...config} />);
+  }
+
 }
 
 export default HexagonGrid;
