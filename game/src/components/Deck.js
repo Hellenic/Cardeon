e420a2e0b189830ad36b79fb749b25a39b560d02
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2016-04-02 14:38:07 +0300

    Deck now somehow displays card data on hexagons

--- game/src/components/Deck.js
+++ game/src/components/Deck.js
@@ -9,8 +9,8 @@ class Deck extends React.Component {
     super(props);
 
     let grid = HexGrid.generate(config.deck);
+    // TODO this.props.actions.saveGrid(grid);
     this.state = {
-      cards: [],
       layout: grid.layout,
       hexagons: grid.hexagons
     };
@@ -19,8 +19,22 @@ class Deck extends React.Component {
   // and assign card per hexagon.
   componentWillMount() {
     CardeonAPI.getCards().then(response => {
+      // Temp filtering
+      let deck = response.data.filter(card => { return card.userId == "2"; });
+      deck = deck.splice(0, 15);
+
+      // TODO this.props.actions.saveDeck(deck);
+      let hexas = this.state.hexagons.map((hex, index) => {
+        let card = deck[index];
+        if (!card)
+          return hex;
+        let img = 'http://localhost:7993/cards/' + card.template.picture;
+        let props = { text: card.name, image: img, id: card.id }
+        return Object.assign({}, hex, { props: props })
+      });
+
       this.setState({
-        cards: response.data
+        hexagons: hexas
       })
     })
   }
